2e189dcf82665e0747add1d66d1ec47274e4a04c
Omer Faruk <omerfarukkocakayaa@gmail.com>
2026-05-20 16:54:23 +0300

    complete phase 1 project setup

---
+++ src/main/java/com/courierflow/CourierflowApplication.java
@@ -0,0 +1,13 @@
+package com.courierflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/courierflow/CourierflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courierflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
